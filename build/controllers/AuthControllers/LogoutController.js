7ad3e6631b98369934c37c91340619b6993926aa
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2021-11-17 21:55:18 +0700

    Delete build directory

--- build/controllers/AuthControllers/LogoutController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// POST: "/logout"
-const Logout = (req, res) => {
-    req.logout();
-    res.status(200).end();
-};
-exports.default = Logout;
